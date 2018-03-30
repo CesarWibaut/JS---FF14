94dfee9d38e1d78be6828bfdaee10c196ffe8c92
Cesar Wibaut <cesar.wibaut@gmail.com>
2018-03-30 02:49:08 +0200

    Basic idea

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hello world");
+function afficher(data){
+    var table = $("#CombatJobs");
+    var res = "";
+    var total=0;
+    var nb = 0;
+    for(var i in data){
+        res += "<tr>";
+        res += "<td><div class=nop>" + data[i].name + "</div></td>";
+        res += "<td>" + data[i].level + "</td>";
+        total += data[i].level;
+        res += "<td>" + data[i].exp.total_percent + "</td>";
+        res += "</td>";
+        nb++;
+    }
+    table.append(res);
+    $("#total").append(total);
+    $("#moyenne").append(total/nb);
+}
+
+$.ajax({
+    type: "GET",
+    url: "https://api.xivdb.com/character/17495202?data=achievements_possibles",
+    dataType: "json",
+    success: function (response) {
+        afficher(response.data.classjobs);
+    }
+});
+
